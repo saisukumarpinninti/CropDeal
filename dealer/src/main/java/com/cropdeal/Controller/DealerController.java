4097eca0d47643eabe694a0a2da725eea8f1b88c
saisukumarpinninti <saisukumarpinniti@gmail.com>
2022-05-11 20:45:54 +0530

    Updated EndPoints

--- dealer/src/main/java/com/cropdeal/Controller/DealerController.java
+++ dealer/src/main/java/com/cropdeal/Controller/DealerController.java
@@ -9,39 +9,41 @@ import java.util.List;
 
 
 @RestController
+@RequestMapping("/Dealer")
 public class DealerController {
 
     @Autowired
     private DealerService DealerService;
 
     //Returns List Of All The Dealers
-    @GetMapping("/Dealers")
+    @GetMapping("/all")
     public List<Dealer> getDealers() {
         return DealerService.getAllDealers();
     }
 
     //Returns the data of The Dealer by using id
-    @GetMapping("/Dealer/{Id}")
+    @GetMapping("/{Id}")
     public Dealer findById(@PathVariable String Id) {
         return DealerService.findById(Id);
     }
 
     //Adds the Dealer into the database
-    @PostMapping("/Dealer/add")
+    @PostMapping("/add")
     public Dealer addDealer(@RequestBody Dealer s) {
         return DealerService.AddDealer(s);
     }
 
     //Updates the Dealer data
-    @PutMapping("/Dealer/update")
+    @PutMapping("/update")
     public Dealer updateDealer(@RequestBody Dealer s) {
         return DealerService.updateDealer(s);
     }
 
     //Deletes the Dealer data by using id
-    @DeleteMapping("/Dealer/delete/{Id}")
+    @DeleteMapping("/delete/{Id}")
     public String deleteDealer(@PathVariable String Id){
         return DealerService.deleteById(Id);
     }
+
 
 }
