339bfe061dd7d8a0717219254d10563099e76ef5
saisukumarpinninti <saisukumarpinniti@gmail.com>
2022-05-13 18:31:05 +0530

    Updated Response Type

--- dealer/src/main/java/com/cropdeal/Controller/DealerController.java
+++ dealer/src/main/java/com/cropdeal/Controller/DealerController.java
@@ -4,6 +4,8 @@ import com.cropdeal.entity.Dealer;
 import com.cropdeal.service.DealerService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,36 +21,65 @@ public class DealerController {
     //Returns List Of All The Dealers
     @GetMapping("/all")
     @ApiOperation(value = "Get all the Dealers List")
-    public List<Dealer> getDealers() {
-        return DealerService.getAllDealers();
+    public ResponseEntity<List<Dealer>> getDealers() {
+        try {
+            return new ResponseEntity<>(DealerService.getAllDealers(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 
     //Returns the data of The Dealer by using id
     @GetMapping("/{Id}")
     @ApiOperation(value = "Get the Dealers Particular Dealer Details By Id")
-    public Dealer findById(@PathVariable String Id) {
-        return DealerService.findById(Id);
+    public ResponseEntity<Dealer> findById(@PathVariable String Id) {
+        try {
+            return new ResponseEntity<>(DealerService.findById(Id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     //Adds the Dealer into the database
     @PostMapping("/add")
     @ApiOperation(value = "Add a New Dealer")
-    public Dealer addDealer(@RequestBody Dealer s) {
-        return DealerService.AddDealer(s);
+    public ResponseEntity<Dealer> addDealer(@RequestBody Dealer s) {
+        try {
+            return new ResponseEntity<>(DealerService.AddDealer(s), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     //Updates the Dealer data
     @PutMapping("/update")
     @ApiOperation(value = "Update an Existing Dealer")
-    public Dealer updateDealer(@RequestBody Dealer s) {
-        return DealerService.updateDealer(s);
+    public ResponseEntity<Dealer> updateDealer(@RequestBody Dealer s) {
+        try {
+            return new ResponseEntity<>(DealerService.updateDealer(s), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 
     //Deletes the Dealer data by using id
     @DeleteMapping("/delete/{Id}")
     @ApiOperation(value = "Delete an existing Dealer By id ")
-    public String deleteDealer(@PathVariable String Id){
-        return DealerService.deleteById(Id);
+    public ResponseEntity<String> deleteDealer(@PathVariable String Id) {
+        try {
+            return new ResponseEntity<String>(DealerService.deleteById(Id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    @GetMapping("/check/{Id}")
+    @ApiOperation(value = "To check the Dealerid is Available or not")
+    public Boolean DealerExits(@PathVariable String Id) {
+        return DealerService.Checkexits(Id);
     }
 
 
--- dealer/src/main/java/com/cropdeal/service/DealerService.java
+++ dealer/src/main/java/com/cropdeal/service/DealerService.java
@@ -1,49 +1,41 @@
 package com.cropdeal.service;
 
 import com.cropdeal.Repository.DealerRepository;
-
 import com.cropdeal.entity.Dealer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Array;
 import java.util.List;
 
 
 /*
-* Service Class For The Farmer
-*/
+ * Service Class For The Farmer
+ */
 @Service
-public class DealerService  {
+public class DealerService {
     @Autowired
     private DealerRepository DealerRepository;
     @Autowired
-    private SequenceGenerator sg ;
+    private SequenceGenerator sg;
 
-    //Empty Addons List
-    //private String[] EmptyAddons ;
 
     public DealerService() {
     }
 
     //Returns List Of All The Dealers
-    public List<Dealer> getAllDealers(){
-        return  DealerRepository.findAll();
+    public List<Dealer> getAllDealers() {
+        return DealerRepository.findAll();
     }
 
     //Returns the data of The Dealer by using id if the Dealer exits or returns Dealer with no data .
-    public Dealer findById(String Id){
+    public Dealer findById(String Id) {
 
-        if(Checkexits(Id)){
-            return DealerRepository.findById(Id).get();
-        }
-        else {
-            return new Dealer();
-        }
+        return DealerRepository.findById(Id).get();
+
     }
 
     //Adds the Dealer into the database
-    public Dealer AddDealer(Dealer F){
+    public Dealer AddDealer(Dealer F) {
 
         //Generating the id of the Dealer
         F.setId(sg.getSequenceNumber("Dealer_Sequence"));
@@ -53,37 +45,26 @@ public class DealerService  {
 
         //ReSetting the addons list to Empty
         //F.setAddons(EmptyAddons);
-
-        return  DealerRepository.save(F);
+        return DealerRepository.save(F);
     }
 
     //Updates the Dealer data in the database if the Dealer exits and returns the updated the data
-    //if the Dealer not exits returns the error
-    public Dealer updateDealer(Dealer F){
-        if(Checkexits(F.getId())){
-            return  DealerRepository.save(F);
-        }
-        else {
-            F.setFirstName("UserNotFound");
-            return F;
-        }
+
+    public Dealer updateDealer(Dealer F) {
+
+        return DealerRepository.save(F);
     }
 
     //Deletes the Dealer data in the database if the Dealer exits and returns the Result
-    //if the Dealer not exits returns the error result
     public String deleteById(String Id) {
 
-        if(Checkexits(Id)){
-            DealerRepository.deleteById(Id);
-            return "Deleted SuccessFully";
-        }
-        else {
-            return "DealerNotFound";
-        }
+        DealerRepository.deleteById(Id);
+        return "Deleted SuccessFully";
+
     }
 
     //checks & Sends if the Dealer exits or not
-    public boolean Checkexits(String Id){
+    public boolean Checkexits(String Id) {
         return DealerRepository.existsById(Id);
     }
 
