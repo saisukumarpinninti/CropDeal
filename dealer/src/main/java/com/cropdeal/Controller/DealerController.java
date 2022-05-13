54dcb72111e60327598d05eb9e7e486edfbde987
saisukumarpinninti <saisukumarpinniti@gmail.com>
2022-05-13 16:33:34 +0530

    Updated Swagger Documentation

--- dealer/src/main/java/com/cropdeal/Controller/DealerController.java
+++ dealer/src/main/java/com/cropdeal/Controller/DealerController.java
@@ -2,6 +2,7 @@ package com.cropdeal.Controller;
 
 import com.cropdeal.entity.Dealer;
 import com.cropdeal.service.DealerService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,30 +18,35 @@ public class DealerController {
 
     //Returns List Of All The Dealers
     @GetMapping("/all")
+    @ApiOperation(value = "Get all the Dealers List")
     public List<Dealer> getDealers() {
         return DealerService.getAllDealers();
     }
 
     //Returns the data of The Dealer by using id
     @GetMapping("/{Id}")
+    @ApiOperation(value = "Get the Dealers Particular Dealer Details By Id")
     public Dealer findById(@PathVariable String Id) {
         return DealerService.findById(Id);
     }
 
     //Adds the Dealer into the database
     @PostMapping("/add")
+    @ApiOperation(value = "Add a New Dealer")
     public Dealer addDealer(@RequestBody Dealer s) {
         return DealerService.AddDealer(s);
     }
 
     //Updates the Dealer data
     @PutMapping("/update")
+    @ApiOperation(value = "Update an Existing Dealer")
     public Dealer updateDealer(@RequestBody Dealer s) {
         return DealerService.updateDealer(s);
     }
 
     //Deletes the Dealer data by using id
     @DeleteMapping("/delete/{Id}")
+    @ApiOperation(value = "Delete an existing Dealer By id ")
     public String deleteDealer(@PathVariable String Id){
         return DealerService.deleteById(Id);
     }
