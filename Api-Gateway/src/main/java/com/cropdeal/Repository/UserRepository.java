34c8eebd0027effc85ad48ed34a4d2577f13def7
saisukumarpinninti <saisukumarpinniti@gmail.com>
2022-05-17 15:19:37 +0530

    Changed Spring boot  parent version and java version

--- Api-Gateway/src/main/java/com/cropdeal/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cropdeal.Repository;
-
-import com.cropdeal.Entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User,String> {
-     User getByname(String name);
-}
--- Api-Gateway/src/main/java/com/techprimers/cloud/ZuulServiceApplication.java
+++ Api-Gateway/src/main/java/com/techprimers/cloud/ZuulServiceApplication.java
@@ -1,17 +1,16 @@
-package com.cropdeal;
+package com.techprimers.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@EnableEurekaClient
 @EnableZuulProxy
-@EnableDiscoveryClient
-public class ApiGatewayApplication {
+@SpringBootApplication
+public class ZuulServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(ZuulServiceApplication.class, args);
 	}
-
 }
